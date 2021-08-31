64ff6dbd34f897cb0f3d94d2bba745e8ad6535e5
FirePlank <44502537+FirePlank@users.noreply.github.com>
2021-08-31 21:58:02 +0300

    first commit

---
+++ butterfingers.py
@@ -0,0 +1,87 @@
+from random import randint
+import random
+import keyboard as k
+import time
+
+def butterfinger(text,prob=0.1,keyboard='qwerty'):
+
+	keyApprox = {}
+	
+	if keyboard == "qwerty":
+		keyApprox['q'] = "qwasedzx"
+		keyApprox['w'] = "wqesadrfcx"
+		keyApprox['e'] = "ewrsfdqazxcvgt"
+		keyApprox['r'] = "retdgfwsxcvgt"
+		keyApprox['t'] = "tryfhgedcvbnju"
+		keyApprox['y'] = "ytugjhrfvbnji"
+		keyApprox['u'] = "uyihkjtgbnmlo"
+		keyApprox['i'] = "iuojlkyhnmlp"
+		keyApprox['o'] = "oipklujm"
+		keyApprox['p'] = "plo['ik"
+
+		keyApprox['a'] = "aqszwxwdce"
+		keyApprox['s'] = "swxadrfv"
+		keyApprox['d'] = "decsfaqgbv"
+		keyApprox['f'] = "fdgrvwsxyhn"
+		keyApprox['g'] = "gtbfhedcyjn"
+		keyApprox['h'] = "hyngjfrvkim"
+		keyApprox['j'] = "jhknugtblom"
+		keyApprox['k'] = "kjlinyhn"
+		keyApprox['l'] = "lokmpujn"
+
+		keyApprox['z'] = "zaxsvde"
+		keyApprox['x'] = "xzcsdbvfrewq"
+		keyApprox['c'] = "cxvdfzswergb"
+		keyApprox['v'] = "vcfbgxdertyn"
+		keyApprox['b'] = "bvnghcftyun"
+		keyApprox['n'] = "nbmhjvgtuik"
+		keyApprox['m'] = "mnkjloik"
+		keyApprox[' '] = " "
+	else:
+		print("Keyboard not supported.")
+		return
+
+	probOfTypo = int(prob * 100)
+
+	double_down = (False, "6969")
+	faster_for_longer = 50 if len(text) >= 10 else 15
+	for letter in text:
+		typo = False
+		lcletter = letter.lower()
+		if not lcletter in keyApprox.keys():
+			newletter = lcletter
+		else:
+			if random.choice(range(0, 100)) <= probOfTypo:
+				newletter = random.choice(keyApprox[lcletter])
+				typo = True
+			else:
+				newletter = lcletter
+
+		if not typo:
+			time.sleep(randint(0,100)/(450+faster_for_longer))
+			k.write(newletter)
+		else:
+			if not double_down[0] and letter != text[-1]:
+				chance = randint(1,3)
+				if chance == 1 or chance == 2:
+					time.sleep(randint(0,100)/450)
+					k.write(newletter)
+					double_down = (True, letter.lower())
+					continue
+				time.sleep(randint(0,100)/(450+faster_for_longer))
+				k.write(newletter)
+				time.sleep(randint(0,100)/400)
+				k.press_and_release('backspace')
+				time.sleep(randint(0,100)/480)
+				k.write(letter.lower())
+		
+		if double_down[0]:
+			time.sleep(randint(0,100)/325)
+			k.press_and_release('backspace')
+			time.sleep(randint(0,100)/525)
+			k.press_and_release('backspace')
+			time.sleep(randint(0,100)/480)
+			k.write(double_down[1])
+			time.sleep(randint(0,100)/(485+faster_for_longer))
+			k.write(letter.lower())
+			double_down = (False, "6969")
---
+++ main.py
@@ -0,0 +1,100 @@
+import pyperclip
+import time
+import keyboard
+import pyautogui
+import random
+import string
+
+from butterfingers import butterfinger
+
+with open("wordlist.txt", "r") as f:
+    content = f.readlines()
+
+used = []
+letters = ""
+found = "6969"
+
+def activate():
+    global letters, used, found
+
+    while 1:
+        x,y = pyautogui.position()
+        pyautogui.moveTo(808, 570) # Change this depending on where the bomb text is on your screen. laptop: 731, 563, pc: 808, 570
+        pyautogui.leftClick()
+        pyautogui.leftClick()
+        before = pyperclip.paste()
+        keyboard.send("ctrl+c")
+        time.sleep(0.1)
+
+        result = pyperclip.paste()
+        time.sleep(0.01)
+        pyperclip.copy(before)
+
+        pyautogui.moveTo(x,y)
+        pyautogui.leftClick()
+
+        search = result.upper().strip()
+        found = ["6969"]
+        top = 0
+        shortest = 696969
+        for i in content:
+            if search in i and i not in used:
+                amount = 0
+                for x in i:
+                    if x not in letters:
+                        amount+=1
+                if amount > top:
+                    found = [i]
+                    top = amount
+                    shortest = len(i)
+                elif amount == top and len(i) < shortest:
+                    found = [i]
+                    top = amount
+                    shortest = len(i)
+                elif amount+4 >= top and len(i)+4 < shortest:
+                    found = [i]
+                    top = amount+4
+                    shortest = len(i)
+                elif amount == top and len(i) == shortest:
+                    found.append(i)
+                    
+            elif i == search and i not in used:
+                found = [i]
+                break
+
+        if found == ["6969"]:
+            print("Did not find any available matches.")
+            used = []
+            continue
+
+        found = random.choice(found)
+
+        used.append(found)
+        butterfinger(found, 0.05)
+        keyboard.press_and_release('enter')
+
+        time.sleep(0.2)
+        try:
+            if pyautogui.pixelMatchesColor(566, 1004, (49, 43, 38)): # Change this depending on where the input text box is on your screen
+                break
+        except:pass
+
+    for i in found:
+        letters+=i
+
+    for i in string.ascii_uppercase:
+        if i not in letters:
+            return
+
+    letters = ""
+    used = []
+
+print("READY!")
+while 1:
+    try:
+        if pyautogui.pixelMatchesColor(788, 1002, (25, 21, 19)): # Change this depending on where the input text box is on your screen
+            time.sleep(random.randint(1,100)/70)
+            activate()
+    except:
+        continue
+    time.sleep(0.15)
